acce405122acacaa6a46d32012d5620668caf400
Greyson Richey <greysonrichey@gmail.com>
2022-06-07 15:01:45 -0600

    Fix movement/collision for units and heros
    - Add player movement for heros
    - Proper lifecycle stuff for units and heros

--- actors/Hero.gd
+++ actors/Hero.gd
@@ -1,7 +1,42 @@
 extends RigidDynamicBody2D
 
+signal died(hero)
+
 @export var data:Resource
 @export var team:int
 
+var alive:bool = true
+
+var _health:float = 0
+var _move_direction:Vector2 = Vector2.ZERO
+
 func damage(amount:float) -> void:
-  print("hero damaged: %.2f" % amount)
+	_health = clampf(_health - (amount - data.armor), 0.0, data.health)
+	
+	if _health <= 0:
+		alive = false
+		died.emit(self)
+		print("hero died")
+	
+func _integrate_forces(state:PhysicsDirectBodyState2D):
+	if _health >= 0:
+		state.linear_velocity = _move_direction * data.speed
+
+func _process(delta):
+	_move_direction = Vector2.ZERO
+
+	if _health > 0:
+		if Input.is_action_pressed("ui_right"):
+			_move_direction += Vector2.RIGHT
+		if Input.is_action_pressed("ui_left"):
+			_move_direction += Vector2.LEFT
+		if Input.is_action_pressed("ui_up"):
+			_move_direction += Vector2.UP
+		if Input.is_action_pressed("ui_down"):
+			_move_direction += Vector2.DOWN
+
+		_move_direction = _move_direction.normalized()
+
+
+func _ready():
+	_health = data.health
--- actors/Unit.gd
+++ actors/Unit.gd
@@ -1,55 +1,67 @@
 extends RigidDynamicBody2D
 
+signal died(unit)
+
 @export var data:Resource
 @export var team:int
+
+var alive:bool = true
 
 @onready var _heros:Array = get_tree().get_nodes_in_group("heros")
 
 var _attack_cooldown:float
 var _health:float
+var _touching_target:bool
 var _target:Node2D
 
 func damage(amount:float) -> void:
-  _health -= amount
+	_health = clampf(_health - (amount - data.armor), 0.0, data.health)
+	
+	if _health <= 0:
+		alive = false
+		died.emit(self)
 
 func _attack(target:Node2D) -> void:
-  target.damage(data.damage)
-  _attack_cooldown = data.attack_interval
+	target.damage(data.damage)
+	_attack_cooldown = data.attack_interval
 
 func _body_entered(body:Node) -> void:
-  print("body entered")
-  if body.is_in_group("heros"):
-    _target = body
-    print("body is now target")
+	if body == _target:
+		_touching_target = true
 
 func _body_exited(body:Node) -> void:
-  if body == _target:
-    _target = null
+	if body == _target:
+		_touching_target = false
 
 func _integrate_forces(state:PhysicsDirectBodyState2D):
-  if _health >= 0:
-    var _direction_vector:Vector2 = global_position.direction_to(_heros[0].global_position)
-    state.linear_velocity = _direction_vector * data.speed
-    
-    state.integrate_forces()
+	if alive && _target != null:
+		var _direction_vector:Vector2 = global_position.direction_to(_target.global_position)
+		state.linear_velocity = _direction_vector * data.speed
 
 func _process(delta):
-  if _health <= 0:
-    queue_free()
-  else:
-    _attack_cooldown = clampf(_attack_cooldown - delta, 0.0, data.attack_interval)
-    
-    if !GDUtil.reference_safe(_target):
-      _target = null
-    
-    if _attack_cooldown <= 0.0 && _target != null:
-      _attack(_target)
-      
+	if alive:
+		_attack_cooldown = clampf(_attack_cooldown - delta, 0.0, data.attack_interval)
+
+		if _attack_cooldown <= 0.0 && _target != null && _touching_target:
+			_attack(_target)
 
 func _ready():
-  body_entered.connect(_body_entered)
-  
-  $"Sprite2D".texture = data.sprite
-  _health = data.health
-  
-  _heros = _heros.filter(func(hero): return hero.team != team)
+	body_entered.connect(_body_entered)
+	body_exited.connect(_body_exited)
+	$"Sprite2D".texture = data.sprite
+	_health = data.health
+
+	_heros = _heros.filter(func(hero): return hero.team != team)
+	_find_target()
+
+func _target_died(hero) -> void:
+	_target.died.disconnect(_target_died)
+	_target = null
+	_find_target()
+
+func _find_target() -> void:
+	var _alive_enemy_heros:Array = _heros.filter(func(hero): return hero.alive)
+
+	if len(_alive_enemy_heros) > 0 && GDUtil.reference_safe(_alive_enemy_heros[0]):
+		_target = _alive_enemy_heros[0]
+		_target.died.connect(_target_died)
